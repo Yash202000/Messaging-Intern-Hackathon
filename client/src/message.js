fd4e15c1da9c974644aca0d0533ee81e7a1fd9fb
hamid murambiwa <machipisajunior@gmail.com>
2022-09-17 18:10:23 +0200

    Correct lint errors

--- client/src/message.js
+++ client/src/message.js
@@ -3,7 +3,7 @@ import './message.css';
 const Message = () => {
 	return (
 		<div className='text-message'>
-			<img src='profile-picture.jpg' alt='profile picture' />
+			<img src='profile-picture.jpg' alt='profile' />
 			<h3>Text here</h3>
 			<p>14:00</p>
 		</div>
